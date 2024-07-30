92da96132478491d9dec5b1a1f8fc8c8ec5a5d27
SirAcia <122296053+SirAcia@users.noreply.github.com>
2024-07-29 21:23:48 -0400

    finished cleaning, need to add comments

--- BTC1855_midterm.R
+++ BTC1855_midterm.R
@@ -7,27 +7,11 @@ library(readr)
 library(funModeling) 
 library(tidyverse) 
 library(Hmisc)
+library(lubridate)
 
 empty_string <- function(col) {
   any(col == "")
 }
-
-rmve_blank <- function(data, column_name) {
-  data[[paste0(column_name, "_alt")]][data[[paste0(column_name, "_fctr")]] ==""] <- NA 
-  #' Redefining levels of factor to remove "" bin as it is empty 
-  new_lvls <- levels(data[[paste0(column_name, "_fctr")]])
-  new_lvls <- new_lvls[new_lvls != ""] 
-  #' Redefining factor with new levels 
-  data[[paste0(column_name, "_fctr")]] <- factor(data[[paste0(column_name, "_fctr")]], levels =   new_lvls)
-}
-
-for (col_name in colnames(trip)) {
-  if (any(col == "") == T){
-    
-    trip[[col_name]][trip[[col_name]] == ""] <- NA
-    
-  }
-} 
 
 basic_eda <- function(data)
 {
@@ -54,11 +38,59 @@ basic_eda(station)
 
 basic_eda(weather)
 
-#Fixing structural issues
 anyNA(trip)
+empty_string(trip)
+
 anyNA(weather)
+empty_string(weather)
 
-#factoring subscription type 
+trip1 <- trip
 
-#finding cancelled trips 
-which(trip$start_station_id == trip$end_station_id && (trip$duration/60) < 3)
+weather1 <- weather 
+
+station1 <- station
+
+for (col_name in colnames(trip1)) {
+  if (any(trip1[[col_name]] == "", na.rm = T)){
+    trip1[[col_name]][trip1[[col_name]] == ""] <- NA
+  }
+} 
+
+for (col_name in colnames(weather1)) {
+  if (any(weather1[[col_name]] == "", na.rm = T)){
+    weather1[[col_name]][weather1[[col_name]] == ""] <- NA
+  }
+} 
+glimpse(trip)
+
+#need to factor start station name, end station name, ids, subscription type, make dates into POSIX
+# change duragion as well 
+
+trip1$end_station_id_fctr <- factor(trip1$end_station_id)
+
+trip1$end_station_name_fctr <- factor(trip1$end_station_name)
+
+trip1$start_station_id_fctr <- factor(trip1$start_station_id)
+
+trip1$start_station_name_fctr <- factor(trip1$start_station_name)
+
+trip1$subscription_type_fctr <- factor(trip1$subscription_type)
+
+trip1$start_date_alt <- mdy_hm(trip1$start_date)
+
+trip1$end_date_alt <- mdy_hm(trip1$end_date)
+
+trip1$duration_alt <- as.POSIXct(trip1$duration)
+
+glimpse(weather)
+
+#nned tio make date as POSIX, factor city, why T in preceiptation --> TRACE AMOUNTS 
+
+weather1$events[weather1$events == "rain"] <- "Rain"
+
+weather1$events_fctr <- factor(weather1$events)
+
+weather1$date_alt <- mdy(weather1$date)
+
+weather1$city_fctr <- factor(weather1$city)
+
